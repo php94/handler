e3bcfa757c5d7af0712629e44b0053fcf7ade152
John Doe <johndoe@example.com>
2024-07-30 16:40:02 +0800

    1

---
+++ src/Handler.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHP94\Handler;
+
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class Handler implements RequestHandlerInterface
+{
+    private $container;
+    private $handler;
+
+    private $middlewares = [];
+
+    public function __construct(
+        ContainerInterface $container,
+        RequestHandlerInterface $requestHandler
+    ) {
+        $this->container = $container;
+        $this->handler = $requestHandler;
+    }
+
+    public function pushMiddleware(...$middlewares): self
+    {
+        array_push($this->middlewares, ...$middlewares);
+        return $this;
+    }
+
+    public function unShiftMiddleware(...$middlewares): self
+    {
+        array_unshift($this->middlewares, ...$middlewares);
+        return $this;
+    }
+
+    public function popMiddleware(): ?MiddlewareInterface
+    {
+        if ($middleware = array_pop($this->middlewares)) {
+            return $this->container->get($middleware);
+        }
+        return null;
+    }
+
+    public function shiftMiddleware(): ?MiddlewareInterface
+    {
+        if ($middleware = array_shift($this->middlewares)) {
+            return $this->container->get($middleware);
+        }
+        return null;
+    }
+
+    public function handle(ServerRequestInterface $serverRequest): ResponseInterface
+    {
+        if ($middleware = self::shiftMiddleware()) {
+            return $middleware->process($serverRequest, $this);
+        } else {
+            return $this->handler->handle($serverRequest);
+        }
+    }
+}
